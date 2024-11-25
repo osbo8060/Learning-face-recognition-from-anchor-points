4139ebc4db65e09ab99e8a5404bfac2511d7ec4a
Boman <71383031+osbo8060@users.noreply.github.com>
2024-11-25 17:12:46 +0100

    training

--- Model/bioNet.py
+++ Model/bioNet.py
@@ -10,6 +10,7 @@ from torch.utils.data import random_split, DataLoader
 from featureNet import FeatureNet
 from metricNet import MetricNet
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # %%
 class BioNet(nn.Module):
     def __init__(self):
@@ -17,16 +18,21 @@ class BioNet(nn.Module):
         self.featureNet = FeatureNet()
         self.metricNet = MetricNet()
     
-    def forward(self, x):
+    def forward(self, img1, img2, batch_size):
         # (2, 3, 299, 299) -> (1, 1024)
-        feature_vector = torch.tensor([])
-        for image in x:
-            image = image.unsqueeze(0)
-            print(image.shape)
-            image_tensor = self.featureNet(image)
-            feature_vector = torch.cat(tensors=(feature_vector, image_tensor), dim=1)
-        print(feature_vector.shape)
-        return self.metricNet(feature_vector)[0]
+        z = torch.zeros(batch_size)
+        i = 0
+        for tensor1, tensor2 in zip(img1,img2):
+
+            feature_vector1 = self.featureNet(tensor1.unsqueeze(0))
+            feature_vector2 = self.featureNet(tensor2.unsqueeze(0))
+        
+            # Concatenate the features along the correct dimension
+            feature_vector = torch.cat((feature_vector1, feature_vector2), dim=1)
+            z_i = self.metricNet(feature_vector)[0]
+            z[i] = z_i
+            i += 1
+        return z
 
 
 
@@ -36,7 +42,7 @@ class BioNetLoss(nn.Module):
     Custom loss function that computes the loss based on the given embeddings.
     Suitable for neural networks.
     """
-    def __init__(self, p):
+    def __init__(self, p, mu_n, mu_m, sigma):
         """
         Initialize the loss function.
 
@@ -45,6 +51,9 @@ class BioNetLoss(nn.Module):
         """
         super(BioNetLoss, self).__init__()
         self.p = p  # Dimensionality of the feature space
+        self.mu_n = mu_n
+        self.mu_m = mu_m
+        self.sigma = sigma
 
     def compute_loss_term(self, mu, z_batch, sigma):
         """
@@ -58,52 +67,58 @@ class BioNetLoss(nn.Module):
         - Loss term value (torch.Tensor).
         """
         # Compute covariance matrice of the samples
-        sigma_batch = torch.var(z_batch, dim=0, unbiased=True)
+        if z_batch.size(0) <= 1:
+            sigma_batch = sigma
         
+        else:
+            sigma_batch = torch.var(z_batch, dim=0, unbiased=True) + 1e-6 #avoid division by 0
+    
         # Compute determinants and inverse
         det_sigma_s = sigma_batch
         inv_sigma = 1/sigma
 
         # Log determinant, first term
         log_det_term = torch.log(sigma / det_sigma_s)
-
         # Trace term, second term
         trace_term = inv_sigma * sigma_batch
 
         # Last term
         diff = mu - torch.mean(z_batch, dim=0)
         lst = diff * inv_sigma * diff  
+        loss = 0.5 * (log_det_term - self.p + trace_term + lst)
+        return loss[0][0]
+    def sort_labels(self, distances, labels):
+    
+        z_n = [z for z, y in zip(distances, labels) if y == 0]
+        z_m = [z for z, y in zip(distances, labels) if y == 1]
 
-        return 0.5 * (log_det_term - self.p + trace_term + lst)
+        z_n = torch.stack(z_n, dim=0) if z_n else torch.empty(0, dtype=torch.float32)
+        z_m = torch.stack(z_m, dim=0) if z_m else torch.empty(0, dtype=torch.float32)
 
-    def forward(self, mu_n, z_sn, mu_m, z_sm, sigma):
+        return z_n, z_m
+    def threshold(self):
+        
+        phi = (self.mu_n + self.mu_m) / 2
+
+        return phi
+    def forward(self, distances, labels):
         """
         Forward pass to compute the total loss.
 
         Parameters:
-        - z_n: Tensor of normal samples (torch.Tensor).
-        - z_sn: Tensor of normal batch samples (torch.Tensor).
-        - z_m: Tensor of abnormal samples (torch.Tensor).
-        - z_sm: Tensor of abnormal batch samples (torch.Tensor).
 
         Returns:
         - Total loss value (torch.Tensor).
         """
-        loss_n = self.compute_loss_term(mu_n, z_sn, sigma)
-        loss_m = self.compute_loss_term(mu_m, z_sm, sigma)
+        
+        z_sn, z_sm = self.sort_labels(distances, labels)
+        loss_n = torch.tensor(0)
+        loss_m = torch.tensor(0)
+        if z_sn.shape[0] > 0:
+            loss_n = self.compute_loss_term(self.mu_n, z_sn, self.sigma)
+        if z_sm.shape[0] > 0:
+            loss_m = self.compute_loss_term(self.mu_m, z_sm, self.sigma)
+
         return loss_n + loss_m
-
-
-def threshold(mu_n, mu_m):
-    """
-    Compute the threshold value for the given embeddings using PyTorch.
-
-    Parameters:
-    - z_n, z_m: Tensors of embeddings (torch.Tensor).
-
-    Returns:
-    - Threshold value (torch.Tensor).
-    """
-    return (mu_n + mu_m) / 2
 
 
